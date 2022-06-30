724fb41e64431b6a8b4501d7ba2b09313582abdd
EhteshamRajpot <muhammadehtesham69@gmail.com>
2022-06-30 23:30:50 +0500

    Updates

---
+++ precache-manifest.9dcfd82a2e5009b15066ee963d47d08b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "42668448847afdf4927d",
+    "url": "/CostumCodeWebsite/static/css/main.d5c41ad4.chunk.css"
+  },
+  {
+    "revision": "42668448847afdf4927d",
+    "url": "/CostumCodeWebsite/static/js/main.42668448.chunk.js"
+  },
+  {
+    "revision": "b31c065368ffec75269b",
+    "url": "/CostumCodeWebsite/static/css/1.8e1c7f35.chunk.css"
+  },
+  {
+    "revision": "b31c065368ffec75269b",
+    "url": "/CostumCodeWebsite/static/js/1.b31c0653.chunk.js"
+  },
+  {
+    "revision": "44ebafd89079d9cff3e6",
+    "url": "/CostumCodeWebsite/static/js/runtime~main.44ebafd8.js"
+  },
+  {
+    "revision": "1ad58db2f6b7a5cf6eb4dd4a7b8153ef",
+    "url": "/CostumCodeWebsite/static/media/logo.1ad58db2.webp"
+  },
+  {
+    "revision": "f433f9740557968ed3ab7c2bf777b8dd",
+    "url": "/CostumCodeWebsite/static/media/liveCoffeMaking.f433f974.webp"
+  },
+  {
+    "revision": "eb13466e2e79b1b856003ffd57849e52",
+    "url": "/CostumCodeWebsite/static/media/sign.eb13466e.webp"
+  },
+  {
+    "revision": "1020be37628a8bd113efe6583393ac24",
+    "url": "/CostumCodeWebsite/static/media/playButton.1020be37.webp"
+  },
+  {
+    "revision": "3b04b40770fb9f3b3d6d489233db924f",
+    "url": "/CostumCodeWebsite/static/media/g1.3b04b407.jpg"
+  },
+  {
+    "revision": "e2a58eb0d0ad2baecc3d93d3ca9f5682",
+    "url": "/CostumCodeWebsite/static/media/g2.e2a58eb0.jpg"
+  },
+  {
+    "revision": "bbb76ad25dfac64feb12a02fcd0eb5a4",
+    "url": "/CostumCodeWebsite/static/media/g3.bbb76ad2.jpg"
+  },
+  {
+    "revision": "adcb1a8c2d09289dfd7abe635c1c74b8",
+    "url": "/CostumCodeWebsite/static/media/g4.adcb1a8c.jpg"
+  },
+  {
+    "revision": "994faada02f0ee1592bcbea181ee619f",
+    "url": "/CostumCodeWebsite/static/media/g5.994faada.jpg"
+  },
+  {
+    "revision": "bc6318915072e05e398c320201bb4ec1",
+    "url": "/CostumCodeWebsite/static/media/logo1.bc631891.webp"
+  },
+  {
+    "revision": "04b38e57cb745f81a7a62b1334b1fd09",
+    "url": "/CostumCodeWebsite/static/media/logo2.04b38e57.webp"
+  },
+  {
+    "revision": "d3aa5c313656a0d315843704de69715c",
+    "url": "/CostumCodeWebsite/static/media/coffee1.d3aa5c31.webp"
+  },
+  {
+    "revision": "4eb45acf7a57912ecc9237b42a5341e5",
+    "url": "/CostumCodeWebsite/static/media/coffee2.4eb45acf.webp"
+  },
+  {
+    "revision": "61f1ab11b5d640cd3aa197d3f5fb7aba",
+    "url": "/CostumCodeWebsite/static/media/super.61f1ab11.webp"
+  },
+  {
+    "revision": "c58afda81be71b55e6da1ea552dfc66e",
+    "url": "/CostumCodeWebsite/static/media/coffee.c58afda8.webp"
+  },
+  {
+    "revision": "0e9150c84f5aad47c777bd5e1ce15b8c",
+    "url": "/CostumCodeWebsite/static/media/footer.0e9150c8.webp"
+  },
+  {
+    "revision": "85a9f04975fa31e49844e36c9c6bc71c",
+    "url": "/CostumCodeWebsite/index.html"
+  }
+];
